eed434b03344dc42bc4a4c6e7416f4235681d6ee
OriginalAlex <ajcartwright.alex288+1@gmail.com>
2017-10-01 13:13:33 +0100

    Added a changeemail command

--- src/main/java/originalalex/github/io/Plugin.java
+++ src/main/java/originalalex/github/io/Plugin.java
@@ -2,6 +2,7 @@ package originalalex.github.io;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import originalalex.github.io.commands.ChangeEmail;
 import originalalex.github.io.commands.Login;
 import originalalex.github.io.commands.Register;
 import originalalex.github.io.events.LoginEvent;
@@ -16,6 +17,7 @@ public class Plugin extends JavaPlugin {
         LoginEvent loginEvent = new LoginEvent(this, db);
         getCommand("login").setExecutor(new Login(loginEvent));
         getCommand("register").setExecutor(new Register(db));
+        getCommand("changeemail").setExecutor(new ChangeEmail(db));
         Bukkit.getServer().getPluginManager().registerEvents(loginEvent, this);
     }
 
